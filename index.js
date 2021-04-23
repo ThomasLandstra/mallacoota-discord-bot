9b572aaeb07402e7ad628e35776a4c69a5c3344c
The Undercover CEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-04-23 17:04:39 +1000

    added Trello Command

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Requirements
 require("dotenv").config();
 const { Client, MessageEmbed, Channel } = require("discord.js");
+const fetch = require('node-fetch');
 
 // Create required variables
 const client = new Client({partials: ["MESSAGE"]});
 const prefix = ".";
+const bugListID = "6070f74555ec8e82fbae4684";
+const bugLabelID = "6070f73dda9ba96c3e7ced5f";
 
 // Start Message
 client.on('ready', () => {
@@ -21,7 +24,7 @@ client.on('message', async message => {
         //Ping Command
         if(cmd === "ping"){
             try {
-                message.channel.send(`<@${message.author.id}> 🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+                message.channel.send(`<@${message.author.id}> 🏓 API Latency is ${Math.round(client.ws.ping)}ms`);
             } catch (error) {
                 try {
                     message.channel.send("Something went wrong");
@@ -33,6 +36,57 @@ client.on('message', async message => {
                 console.log(`Author: ${message.author.id}, In: ${message.channel.id}`);
                 console.log(error);
             }
+        } else if(cmd == "bug"){
+            try {
+                message.channel.send(`<@${message.author.id}>, attempting to add bug to Trello.`);
+            } catch (error) {
+                try {
+                    message.channel.send("Something went wrong");
+                } catch (error){
+                    console.log("Error sending error message for bug command: " + Date());
+                    console.log(error);
+                }
+                console.log("Error sending message for bug command: " + Date());
+                console.log(`Author: ${message.author.id}, In: ${message.channel.id}`);
+                console.log(error);
+            }
+            fetch(`https://api.trello.com/1/cards?key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}&name=${args.join(" ")}&pos=top&idList=${bugListID}&idLabels=${bugLabelID}`, {
+                method: 'POST'
+            })
+            .then(response => {
+                console.log(
+                `Response for adding card to trello: ${response.status} ${response.statusText}`
+                );
+                try {
+                    message.channel.send(`<@${message.author.id}>, bug added to Trello.`);
+                } catch (error) {
+                    try {
+                        message.channel.send("Something went wrong. Bug added to Trello tho so...");
+                    } catch (error){
+                        console.log("Error sending error message for bug command success: " + Date());
+                        console.log(error);
+                    }
+                    console.log("Error sending message for bug command success: " + Date());
+                    console.log(`Author: ${message.author.id}, In: ${message.channel.id}`);
+                    console.log(error);
+                }
+                return response.text();
+            })
+            .then(text => console.log(text))
+            .catch(err => {
+                console.error(err);
+                try {
+                    message.channel.send("Something went wrong. Bug not added to Trello.");
+                } catch (error){
+                    console.log("Error sending error message for bug command Trello Failure: " + Date());
+                    console.log(error);
+                }
+                console.log("Error sending message for bug command Trello Failure: " + Date());
+                console.log(`Author: ${message.author.id}, In: ${message.channel.id}`);
+                console.log(error);
+            });
         }
     }
 })
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
